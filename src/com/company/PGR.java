f0e5952a8f687b451f6ae533e1153c0e98218f0b
mikjab <107122539+mikjab@users.noreply.github.com>
2022-06-08 14:52:55 +0200

    Add files via upload
    odpowiednie działanie raportu

--- src/com/company/PGR.java
+++ src/com/company/PGR.java
@@ -6,13 +6,17 @@ public class PGR extends Farmer {
     public String simulate(Garden g, int time) throws InterruptedException {
         long startTime=System.currentTimeMillis();
         StringBuilder report= new StringBuilder("Raport: \n");
+        report.append("warzywa: \n");
+        StringBuilder warzywa=new StringBuilder();
+        int value=0;
         while((System.currentTimeMillis()-startTime)/1000<time){
             Thread.sleep(11000);
             for(int i=0;i<g.getNumber_of_slots();i++){
                 String temp=g.harvest(i);
+                String[] infoAboutVegetable=temp.split(" ");
+                warzywa.append(infoAboutVegetable[1]).append("\n");
+                value+=Integer.parseInt(infoAboutVegetable[2]);
                 System.out.println(temp);
-                report.append(temp);
-                report.append("\n");
             }
         }
         return report.toString();
